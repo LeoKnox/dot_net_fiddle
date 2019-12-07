c5d0ac9714fc5e20b21f0aed1387b886484d8d63
LeoKnox <LeoKnox@users.noreply.github.com>
2019-12-06 22:02:48 -0800

    Create test1.cs

---
+++ test1.cs
@@ -0,0 +1,27 @@
+using System;
+					
+public class Program
+{
+	public class Test
+	{
+		public string Name { get; set; }
+		public int Str { get; set; }
+		
+		public Test(string name, int str)
+		{
+			Name = name;
+			Str = str;
+		}
+		
+		public int Boost()
+		{
+			return(Str+3);
+		}
+	}
+	public static void Main()
+	{
+		Test name1 = new Test("chuck", 12);
+		Console.WriteLine("Hello {0}", name1.Name);
+		Console.WriteLine("{0} boosts strength from {1} to {2}", name1.Name, name1.Str, name1.Boost());
+	}
+}
