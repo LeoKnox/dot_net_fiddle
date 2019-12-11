e654af4885b60a8645f7dd0e2443f399ad07ab2b
LeoKnox <LeoKnox@users.noreply.github.com>
2019-12-10 21:52:07 -0800

    Update interface_two.cs

--- interface_two.cs
+++ interface_two.cs
@@ -16,11 +16,13 @@ public class Program
 	
 	class Character : Sword, FireSpell
 	{
+		public string CharacterName { get; set; }
 		public string SwordType { get; set; }
 		public string FireType { get; set; }
 		
-		public Character(string EssTee, string FffTee)
+		public Character(string charname, string EssTee, string FffTee)
 		{
+			this.CharacterName = charname;
 			this.SwordType = EssTee;
 			this.FireType = FffTee;
 		}
@@ -37,7 +39,7 @@ public class Program
 	
 	public static void Main()
 	{
-		Character Aelien = new Character("Long Sword", "FireBall");
-		Console.WriteLine("Aelien swings {0} for {1}", Aelien.SwordType, Aelien.SwordDamage());
+		Character player1 = new Character("Aelien", "Long Sword", "FireBall");
+		Console.WriteLine("{0} swings {1} for {2} damage! and casts {3} for {4} damage!", player1.CharacterName, player1.SwordType, player1.SwordDamage(), player1.FireType, player1.FireDamage());
 	}
 }
