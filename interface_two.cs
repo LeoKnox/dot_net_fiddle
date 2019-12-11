951133769966065566e26f68a7b6a0ca26413243
LeoKnox <LeoKnox@users.noreply.github.com>
2019-12-10 22:04:35 -0800

    Update interface_two.cs

--- interface_two.cs
+++ interface_two.cs
@@ -37,9 +37,28 @@ public class Program
 		}
 	}
 	
+	class Monster
+	{
+		public String Name { get; set; }
+		private int HP { get; set; }
+		
+		public Monster(string name)
+		{
+			this.Name = name;
+			this.HP = 5;
+		}
+		
+		public int TakeDamage(int dam)
+		{
+			return this.HP - dam;
+		}
+	}
+	
 	public static void Main()
 	{
 		Character player1 = new Character("Aelien", "Long Sword", "FireBall");
+		Monster monster1 = new Monster("Ord");
 		Console.WriteLine("{0} swings {1} for {2} damage! and casts {3} for {4} damage!", player1.CharacterName, player1.SwordType, player1.SwordDamage(), player1.FireType, player1.FireDamage());
+		Console.WriteLine("{0} takes {1} damage! and now has {2} HP!", monster1.Name, player1.SwordDamage(), monster1.TakeDamage(player1.SwordDamage()));
 	}
 }
