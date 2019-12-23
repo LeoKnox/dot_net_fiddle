795298ab7443c3359219d6ce703871a7c3c8fbc8
LeoKnox <LeoKnox@users.noreply.github.com>
2019-12-22 20:30:28 -0800

    Update ClassWList.cs

--- ClassWList.cs
+++ ClassWList.cs
@@ -1,12 +1,32 @@
+using System;
+
+using System.Collections.Generic;
+
+                                                                               
+
 public class Program
 
 {
+
+               
 
                 public class helix
 
                 {
 
+                                public int height { get; set; }
+
                                 public List<int> parts = new List<int>();
+
+                               
+
+                                public int first()
+
+                                {
+
+                                                return this.parts[0];
+
+                                }
 
                 }
 
@@ -18,11 +38,23 @@ public class Program
 
                                 //List<helix> parts = new List<helix>();
 
+                                List<int> addme = new List<int>();
+
+                                addme.Add(1);
+
+                                addme.Add(3);
+
+                                addme.Add(4);
+
                                 helix piece = new helix();
+
+                                piece.parts = addme;
+
+                                piece.height = 4;
 
                                 piece.parts.Add(3);
 
-                                Console.WriteLine("Hello World" + piece.parts[0]);
+                                Console.WriteLine("Hello World " + piece.first());
 
                 }
 
