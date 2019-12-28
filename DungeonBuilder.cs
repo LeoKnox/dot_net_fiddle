de7bd1f19be8f2de0d7b7428d920d55bfecb8e10
LeoKnox <LeoKnox@users.noreply.github.com>
2019-12-27 19:37:54 -0800

    Update DungeonBuilder.cs

--- DungeonBuilder.cs
+++ DungeonBuilder.cs
@@ -32,15 +32,44 @@ public class Program
 
                 }
 
+                public class Hall
+
+                {
+
+                                public int length;
+
+                                public int width;
+
+                                public int? breaks;
+
+                               
+
+                                public Hall(int l, int w, int? b)
+
+                                {
+
+                                                this.length = l;
+
+                                                this.width = w;
+
+                                                this.breaks = b;
+
+                                }
+
+                }
+
                 public static void Main()
 
                 {
 
                                 Room first = new Room();
 
+                                Hall fway = new Hall(5, 2, 3);
+
                                 Console.WriteLine("Hello World {0}", first.MakeRoom().width);
+
+                                Console.WriteLine("Hall {0} - {1} - {2}", fway.length, fway.width, fway.breaks);
 
                 }
 
 }
-
