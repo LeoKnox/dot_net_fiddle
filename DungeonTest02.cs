06b3ffd02ccc12009b687d536340c3952a1170c5
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-04 10:17:18 -0800

    Update DungeonTest02.cs

--- DungeonTest02.cs
+++ DungeonTest02.cs
@@ -5,14 +5,17 @@ public class Program
 {
 	public class Room
 	{
-		public int h;
-		public int w;
+		public int height;
+		public int width;
+		public int length;
+		public List<string> items = new List<string>();
 		public List<string> mobs = new List<string>();
 		
-		public Room(int hh, int ww)
+		public Room(int hh, int ww, int ll)
 		{
-			this.h = hh;
-			this.w = ww;
+			this.height = hh;
+			this.width = ww;
+			this.length = ll;
 		}
 		
 		public void AddMob(string x)
@@ -23,14 +26,14 @@ public class Program
 	
 	public static void Main()
 	{
-		Room newdun = new Room(3, 4);
+		Room newdun = new Room(8, 4, 5);
 		newdun.AddMob("Orc");
 		newdun.AddMob("Goblin");
-		Console.Write("Room is {0} by {1} and contains", newdun.h, newdun.w);
+		Console.Write("Room is {0} by {1} and contains", newdun.length, newdun.width);
 		foreach (var mob in newdun.mobs)
 		{
 			Console.Write(" {0} and", mob);
 		}
-		Console.WriteLine("more!");
+		Console.WriteLine(" more!");
 	}
 }
