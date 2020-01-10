c0a1038aa8917fd08139e2209c02c4f722be73ed
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-09 20:27:50 -0800

    Update View.cs

--- mvc_class/View.cs
+++ mvc_class/View.cs
@@ -3,6 +3,26 @@
 var room = ViewData.Model;
 }
 
-  <p>
-@room.Width
+<body>
+<div class="container">
+<div class="col-md-6 col-md-offset-3">
+<h1>Hello Stranger</h1>
+
+<button type="button" class="btn btn-success submit">Ask</button>
+<p>
+Room Width: @room.Width Height: @room.Height
 </p>
+<div>
+@foreach (var mo in room.Mobs)
+{
+<p>@mo</p>
+}
+</div>
+<br/><br/>
+<div class="alert alert-warning fade">
+<img src="http://entechprod.blob.core.windows.net/dotnetfiddle/morpheus.jpg" style="max-width:100%;"/><br/><br/>
+<strong><span class="alert-content"></span></strong>
+</div>
+</div>
+</div>
+</body>
