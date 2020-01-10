795fd787bcfd9e6c7737090a7341428e63a36a51
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-09 20:26:32 -0800

    Update Model.cs

--- mvc_class/Model.cs
+++ mvc_class/Model.cs
@@ -1,5 +1,19 @@
+model
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelloWorldMvcApp
+{
 public class Room
 {
 public int Width { get; set; }
 public int Height { get; set; }
+public List<string> Mobs = new List<string>();
+
+public void AddMob(string mob)
+{
+this.Mobs.Add(mob);
+}
+}
 }
