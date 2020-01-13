16f4731554a27bb7352d1cccffdb8cd0cf2b3b5b
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-12 20:45:03 -0800

    Update Dictionary.cs

--- Dictionary.cs
+++ Dictionary.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 					
 public class Program
 {
+	public class Strs
+	{
+		public string a { get; set; }
+		public string b { get; set; }
+		
+		public Strs(string a, string b)
+		{
+			this.a = a;
+			this.b = b;
+		}
+	}
+	
 	public static void Main()
 	{
 		Dictionary<string, int> nums = new Dictionary<string, int>();
+		Dictionary<int, Strs> lets = new Dictionary<int, Strs>();
+		Strs bbb = new Strs("Three", "Four");
+		lets[1] = bbb;
 		nums.Add("One", 1);
 		nums.Add("Two", 2);
 		nums["Three"] = 3;
 		nums["Four"] = 4;
 		Console.WriteLine("Hello World " + nums["One"]);
+		Console.WriteLine(lets[1].a + " " + lets[1].b);
 	}
 }
