dd2a91c71e60321e70f4ca2aaa2cf1188e5913f5
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-13 22:15:00 -0800

    Create ObjectArray.cs

---
+++ ObjectArray.cs
@@ -0,0 +1,12 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		object[] array = new object[2];
+		array[0] = "cool";
+		array[1] = -4;
+		Console.WriteLine("Hello World");
+	}
+}
