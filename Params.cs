c90c1123fce8c9e0b5781884dd9359c6b7bd6766
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-13 22:25:36 -0800

    Create Params.cs

---
+++ Params.cs
@@ -0,0 +1,21 @@
+using System;
+					
+public class Program
+{
+	public static void UseParams(params object[] list)
+	{
+		for (int i = 0; i < list.Length; i++)
+		{
+			Console.WriteLine(list[i]);
+		}
+	}
+	
+	public static void Main()
+	{
+		UseParams(3, "five", 10.2f);
+		object[] array = new object[2];
+		array[0] = "cool";
+		array[1] = -4;
+		Console.WriteLine("Hello World");
+	}
+}
