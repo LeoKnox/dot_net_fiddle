48d632dc3b092a901197b45c312a8a1ec0889447
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-15 21:25:15 -0800

    Create DictionaryVariable.cs

---
+++ DictionaryVariable.cs
@@ -0,0 +1,28 @@
+using System;
+
+using System.Collections.Generic;
+
+                                                                               
+
+public class Program
+
+{
+
+                public static void Main()
+
+                {
+
+                                Dictionary<string, int> words = new Dictionary<string, int>();
+
+                                words["one"] = 1;
+
+                                words["two"] = 2;
+
+                                string a = "one";
+
+                                Console.WriteLine("Hello World " + words[a]);
+
+                }
+
+}
+
