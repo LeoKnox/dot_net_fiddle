cea4bef8fe573b8450ec60f6735463e86fc6b302
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-18 21:30:02 -0800

    Update Array2.cs

--- Array2.cs
+++ Array2.cs
@@ -5,7 +5,7 @@ public class Program
 	public class Runner
 	{
 		public string Name { get; set; }
-		pbulic string Type { get; set; }
+		public string Type { get; set; }
 		public int Use { get; set; }
 		
 		public Runner(string n, string t, int u)
@@ -18,7 +18,7 @@ public class Program
 	
 	public static void Main()
 	{
-		Runner test = new test("Howard", "Red", 13);
+		Runner test = new Runner("Howard", "Red", 13);
 		string[] names = { "Strange", "Wayne", "Tree" };
 		int[] stats = new int[7];
 		Console.WriteLine("Hello World");
