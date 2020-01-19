9f50f983a5e1c5a640c625c17f80c60bf683f123
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-18 21:28:57 -0800

    Update Array2.cs

--- Array2.cs
+++ Array2.cs
@@ -2,8 +2,23 @@ using System;
 					
 public class Program
 {
+	public class Runner
+	{
+		public string Name { get; set; }
+		pbulic string Type { get; set; }
+		public int Use { get; set; }
+		
+		public Runner(string n, string t, int u)
+		{
+			this.Name = n;
+			this.Type = t;
+			this.Use = u;
+		}
+	}
+	
 	public static void Main()
 	{
+		Runner test = new test("Howard", "Red", 13);
 		string[] names = { "Strange", "Wayne", "Tree" };
 		int[] stats = new int[7];
 		Console.WriteLine("Hello World");
