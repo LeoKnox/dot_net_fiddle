8f552c1d3404743abff00a3ba6616bd9ecf093f6
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-23 22:16:33 -0800

    Create Switch.cs

---
+++ Switch.cs
@@ -0,0 +1,16 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string Test = "one";
+		switch (Test)
+		{
+			case "one":
+				Console.WriteLine(Test);
+				break;
+		}
+		Console.WriteLine("Hello World");
+	}
+}
