44404efa46fbd329792dc202e142b7672fec938a
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-24 21:15:47 -0800

    Update Room.cs

--- roooms/Room.cs
+++ roooms/Room.cs
@@ -1,13 +1,21 @@
 using System;
+using System.Collections.Generic;
 					
 public class Program
 {
+	public class Door
+	{
+		int wall { get; set; }
+		int pos { get; set; }
+	}
+	
 	public class Room
 	{
 		int length { get; set; }
 		int width { get; set; }
 		int locx { get; set; }
 		int locy { get; set; }
+		List<Door> doors = new List<Door>();
 	}
 	
 	public static void Main()
