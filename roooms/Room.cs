3e4d01919bcc81db287fed8087ead07cb3b3905c
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-24 21:06:04 -0800

    Create Room.cs

---
+++ roooms/Room.cs
@@ -0,0 +1,17 @@
+using System;
+					
+public class Program
+{
+	public class Room
+	{
+		int length { get; set; }
+		int width { get; set; }
+		int locx { get; set; }
+		int locy { get; set; }
+	}
+	
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+	}
+}
