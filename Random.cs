cffa67734232135320b36b2f8b07564ecc6eae0d
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-15 00:07:05 -0800

    Update Random.cs

--- Random.cs
+++ Random.cs
@@ -5,12 +5,25 @@ public class Program
 	public static void Main()
 	{
 		Random random = new Random();
-        Console.WriteLine("RANDOM6: " + random.Next(1, 7));
-        Console.WriteLine("RANDOM6: " + random.Next(1, 7));
-        Console.WriteLine("RANDOM6: " + random.Next(1, 7));
+        int ac = random.Next(10, 18);
+		Console.WriteLine("ac = " + ac);
+		int sumy = 0;
+		int sumz = 0;
 		for (int i = 0; i < 10; i++)
 		{
-			Console.WriteLine("RANDOM: " + random.Next(1, 21));
+			int x = random.Next(1, 21);
+			int y = random.Next(1, 13);
+			int z = random.Next(1, 7) + random.Next(1, 7);
+			if (x > ac)
+			{
+				Console.WriteLine("RANDOM: " + x);
+				Console.WriteLine("1d12: " + y);
+				Console.WriteLine("2d6: " + z);
+				sumy += y;
+				sumz += z;
+			}
 		}
+		Console.WriteLine(sumy);
+		Console.WriteLine(sumz);
 	}
 }
